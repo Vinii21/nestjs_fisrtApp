beac8547fdcab71b7de38554ae838fc163b0c8f7
Vinicio Chavarria <vichavarria21@gmaail.com>
2024-09-19 08:13:08 -0600

    fixed in main.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { ConfigModule } from '@nestjs/config';
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_URI, {
-      dbName: process.env.MONGO_DBD_NAME,
+      dbName: process.env.MONGO_DB_NAME,
     }),
     AuthModule
   ],
